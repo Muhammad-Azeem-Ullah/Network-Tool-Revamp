4f203a03025ad9218fd7a534f0e3de7a5e0ec94f
umeridrees <umer@cloudcade.pk>
2018-11-26 17:28:21 +0500

    used echarts

--- Controllers/mongo-controller.js
+++ Controllers/mongo-controller.js
@@ -1,77 +1,407 @@
-
-var http              = require('http');
-var MongoClient       = require('mongodb').MongoClient;
-var randomString      = require('random-string');
-var url               = "mongodb://localhost:27017/";
-var express           = require( "express" );
+var http = require('http'),
+  MongoClient = require('mongodb').MongoClient,
+  mikroNode = require('mikronode'),
+  handleError = require('errorhandler');
 
 
-var database_object;
-var database_coupons_object; 
-var database_name;
-var db_coupon_collection;
+var url = "mongodb://localhost:27017/";
+const sizeRequestsCollection = 100; // Value is in GBs
 
 
+var databaseObject;
+var networkDnsLog;
+var networkUserDetails;
+var networkUserRequestDetails;
 
- module.exports.create_connection =  function create_connnect_with_mongo( db_name , db_collection )
-{
-  MongoClient.connect(url, function(err, db) {
+
+var routerSubMaskIp = '192.168.8';
+var routerIp = routerSubMaskIp + '0.1';
+var mikroTipObject = new mikroNode(routerIp);
+
+var speedTable = {};
+var downloadMap = {};
+
+////////////////////////////////////////
+////////////////////////////////////////
+//@Since 1.0 ///////////////////////////
+//Module exports for mongodb interaction
+module.exports.createConnection = function createConnnectWithMongo(dbName, networkDnsLog_, networkUserDetails_, networkUserRequestDetails_, resolve_callback) {
+
+  MongoClient.connect(url, {
+    useNewUrlParser: true
+  }, function (err, db) {
+
     if (err) throw err;
-    database_object = db.db( db_name );
-    database_object.createCollection( db_collection , function(err, res) {
+
+    databaseObject = db.db(dbName);
+
+    databaseObject.createCollection(networkDnsLog_, function (err, res) {
+
+      if (err) throw err;
+
     });
-    database_name         = db_name;
-    db_coupon_collection  = db_collection;
-  });
-}
+    databaseObject.createCollection(networkUserDetails_, function (err, res) {
 
-module.exports.save_generated_coupons =  function save_generated_coupons( coupons )
-{  
-  database_object.collection( db_coupon_collection ).insertMany( coupons , function(err, res) {
-      if (err) throw err;  
-    });
-}
-
-module.exports.get_all_coupons =  function get_all_coupons( ws )
-{  
-  database_object.collection( db_coupon_collection ).find({}).limit( 500 ).toArray(function(err, all_coupons) {
-    if (err) throw err;  
-
-    var output = "";
-    
- 
-    all_coupons.forEach(function(element) {
-      output += element.name + "<br>";
+      if (err) throw err;
+      databaseObject.collection(networkUserDetails_).createIndex({
+        ipAddress: 1
       });
-      ws.send(JSON.stringify({
-        mode : 'redeem',
-        status : '200' ,
-        response : output
-      }));
 
-  });
-}
+    });
+    databaseObject.createCollection(networkUserRequestDetails_, function (err, res) {
 
-module.exports.get_all_specefic_coupons =  function get_all_specefic_coupons( number_of_coupons  )
-{  
-  database_object.collection( db_coupon_collection ).find({}).limit( number_of_coupons ).toArray(function(err, all_coupons) {
-    if (err) throw err;
-    return all_coupons;
-  });
-}
+      if (err) throw err;
 
-module.exports.delete_coupon =  function delete_coupon( delete_coupon  )
-{
-  database_object.collection("Coupons").find(delete_coupon).toArray(function(err, result) {
-    if (err) throw err;
-    if( result.length > 0 )
-    {
-      database_object.collection( db_coupon_collection ).deleteOne(delete_coupon, function(err, obj) {
+    });
+
+
+    databaseName = dbName;
+    networkDnsLog = networkDnsLog_;
+    networkUserDetails = networkUserDetails_;
+    networkUserRequestDetails = networkUserRequestDetails_;
+
+
+    setInterval(function () {
+      databaseObject.collection(networkUserRequestDetails_).stats(function (err, results) {
+
+        if (results.storageSize > sizeRequestsCollection * 1024 * 1024) // one row contains 70 bytes
+        {
+          databaseObject.collection(networkUserRequestDetails_).find().skip(parseInt(sizeRequestsCollection * 1024 * 1024 / (results.storageSize / results.count))).sort({
+            searchnonce: 1
+          }).toArray(function (err, result_b) {
+            if (result_b) {
+              console.log("second doing");
+              databaseObject.collection(networkUserRequestDetails_).removeMany({
+                searchnonce: {
+                  $gt: result_b[0].searchnonce
+                }
+              });
+            }
+
+          });
+        }
+
+      });
+      databaseObject.collection(networkDnsLog_).stats(function (err, results) {
+
+        if (results.storageSize > (50 * 1024 * 1024)) // one row contains 70 bytes
+        {
+          databaseObject.collection(networkDnsLog_).find().map(function (i) {
+            return i._id;
+          }).skip(parseInt(25 * 1024 * 1024 / (results.storageSize / results.count))).sort({
+            searchnonce: 1
+          }).toArray(function (err, result_b) {
+            if (result_b) {
+              console.log(result_b.length);
+              console.log("i 'm doing");
+              console.log(results.storageSize);
+              databaseObject.collection(networkDnsLog_).deleteMany({
+                '_id': {
+                  '$in': result_b
+                }
+              })
+            }
+
+          });
+        }
+
+      });
+
+    }, 10000);
+
+
+    /////////////////////////////////////////////////////umer graph/////////////////////////////////
+    setInterval(function () {
+      databaseObject.collection(networkUserDetails).find({}).limit(500).toArray(function (err, allUserDetails) {
+
         if (err) throw err;
-      })
-    }  
- 
- 
+        if (allUserDetails.length > 0) {
+          //do async later
+          for (let i = 0; i < allUserDetails.length; i++) {
+            //make option array table here
+            if (typeof speedTable[allUserDetails[i].ipAddress] != 'undefined') {
 
-});
+              if (speedTable[allUserDetails[i].ipAddress].length == 31) {
+                speedTable[allUserDetails[i].ipAddress].shift();
+              }
+              speedTable[allUserDetails[i].ipAddress].push((allUserDetails[i].totalDownload - downloadMap[allUserDetails[i].ipAddress]) * 8 / (1024 * 1024));
+              downloadMap[allUserDetails[i].ipAddress] = allUserDetails[i].totalDownload;
+
+            } else {
+
+              speedTable[allUserDetails[i].ipAddress] = [];
+              speedTable[allUserDetails[i].ipAddress].push(0);
+              downloadMap[allUserDetails[i].ipAddress] = allUserDetails[i].totalDownload;
+
+            }
+          }
+        }
+      });
+    }, 1000);
+/////////////////////////////////////////////////////umer graph end///////////////////////////////////
+    resolve_callback('fine');
+  });
+
+}
+
+
+module.exports.saveDnsDetails = function saveDnsDetailsModule(dnsDetails) {
+  databaseObject.createCollection(networkDnsLog, function (err, res) {
+    if (err) throw err;
+    databaseObject.collection(networkDnsLog).insertMany(dnsDetails, function (err, res) {
+      if (err) throw err;
+    });
+  });
+}
+module.exports.getDnsAddress = function getDnsAddress(ipAddress, resolve) {
+
+  databaseObject.collection(networkDnsLog).find({
+    'ipAddress': ipAddress
+  }).toArray(function (err, result) {
+
+    if (err) throw err;
+    if (result.length > 0) {
+
+      resolve({
+        'dsnUrl': result[0].urlLink
+      });
+
+    } else {
+
+      databaseObject.collection(networkDnsLog).find({
+        'ipAddress': ipAddress
+      }).toArray(function (err, result) {
+
+        if (result.length > 0) {
+          resolve({
+            'dsnUrl': result[0].urlLink
+          });
+        } else {
+          resolve({
+            'dsnUrl': ipAddress
+          });
+        }
+
+      });
+
+    }
+  });
+}
+
+module.exports.getDnsCollection = function getDnsCollection(resolve) {
+  databaseObject.collection(networkDnsLog).find().toArray(function (err, result) {
+
+    if (err) throw err;
+    resolve({
+      'dsnCollection': result
+    });
+
+  });
+
+}
+
+
+module.exports.saveUserDetails = function saveUserDetails(userDetail) {
+
+  if (userDetail.type === 'Download') {
+
+    userEntry = {
+      '_id': userDetail.ipAddress,
+      'ipAddress': userDetail.ipAddress,
+      'ipName': userDetail.ipName,
+      'totalUpload': 0,
+      'totalDownload': userDetail.totalSz,
+    };
+
+  } else {
+
+    userEntry = {
+      '_id': userDetail.ipAddress,
+      'ipAddress': userDetail.ipAddress,
+      'ipName': userDetail.ipName,
+      'totalUpload': userDetail.totalSz,
+      'totalDownload': 0,
+    };
+
+  }
+  databaseObject.collection(networkUserRequestDetails).insertMany([userDetail], function (err, res) {
+    if (err) throw err;
+  });
+
+  databaseObject.collection(networkUserDetails).findOneAndUpdate({
+    'ipAddress': userEntry.ipAddress
+  }, {
+    $inc: {
+      "totalUpload": userEntry.totalUpload,
+      "totalDownload": userEntry.totalDownload
+    },
+    $set: {
+      "ipName": userEntry.ipName
+    }
+  }, {
+    upsert: true,
+  });
+
+}
+module.exports.saverequestDetails = function saverequestDetails(requestDetails) {
+
+  databaseObject.collection(networkUserRequestDetails).insertMany(requestDetails, function (err, res) {
+    if (err) throw err;
+  });
+
+}
+module.exports.getAllUserDetails = function getAllUserDetails(res) {
+
+  databaseObject.collection(networkUserDetails).find({}).limit(200).toArray(function (err, allUserDetails) {
+
+    if (err) throw err;
+    res.render("tables", {
+      "listObj": allUserDetails
+    });
+    return allUserDetails;
+
+  });
+
+}
+
+module.exports.getAllUserDetailsToGraph = function getAllUserDetailsToGraph(res) {
+
+  let graphArray = [];
+
+  for (const key in speedTable) {
+    graphArray.push({
+      name: key,
+      type: 'line',
+      data: speedTable[key],
+      areaStyle: {
+        normal: {}
+      },
+      smooth: true
+    });
+  }
+
+  res.sendUTF(
+    JSON.stringify({
+      animation: false,
+
+      tooltip: {
+        trigger: 'item',
+        axisPointer: {
+          type: 'cross',
+          label: {
+            backgroundColor: '#6a7985'
+          }
+        },
+        triggerOn: 'mousemove|click'
+      },
+
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+
+      xAxis: [{
+        type: 'category',
+        boundaryGap: false,
+        data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+      }],
+
+      yAxis: [{
+        type: 'value',
+        name: 'Speed/Mbps',
+        min: 0.1
+      }],
+
+      series: graphArray
+
+    })
+  );
+}
+
+module.exports.getAllUserRequestDetails = function getAllUserRequestDetails(ipAddress, res) {
+
+  databaseObject.collection(networkUserRequestDetails).find({
+    'ipAddress': ipAddress
+  }).limit(200).toArray(function (err, allUserRequestDetails) {
+    if (err) throw err;
+    res.render("user", {
+      "listObj": allUserRequestDetails
+    });
+    return allUserRequestDetails;
+
+  });
+
+}
+
+module.exports.getAllUserRequestDetailsByIp = function getAllUserRequestDetailsByIp(ipAddress, timestamp, webSocket) {
+  databaseObject.collection(networkUserRequestDetails).find({
+    $and: [{
+      'searchnonce': {
+        $gte: timestamp
+      }
+    }, {
+      'ipAddress': ipAddress
+    }]
+  }).limit(500).toArray(function (err, result) {
+
+    if (err) throw err;
+    if (result.length > 0) {
+
+
+      data = JSON.stringify({
+        'Data': result
+      });
+      webSocket.send(data, function data(err) {
+        if (err) handleError(err);
+      });
+
+    } else {
+
+      data = JSON.stringify({
+        'Data': 'noData'
+      });
+      webSocket.send(data, function data(err) {
+        if (err) handleError(err);
+      });
+
+    }
+
+
+  });
+}
+
+module.exports.getAllUserDetailsWs = function getAllUserDetailsWs(webSocket) {
+  databaseObject.collection(networkUserDetails).find().limit(500).toArray(function (err, result) {
+
+    if (err) throw err;
+    if (result.length > 0) {
+      data = JSON.stringify({
+        'Data': result
+      });
+      webSocket.send(data, function data(err) {
+        if (err) handleError(err);
+      });
+    } else {
+      data = JSON.stringify({
+        'Data': 'noData'
+      });
+      webSocket.send(data, function data(err) {
+        if (err) handleError(err);
+      });
+    }
+
+
+  });
+}
+module.exports.removeCollection = function removeCollection() {
+
+  databaseObject.collection(networkUserDetails_).drop();
+  databaseObject.collection(networkUserDetails_).find({}).limit(10).toArray(function (err, all_coupons) {
+    if (err) throw err;
+    process.exit();
+  });
+
+
 }
--- server.js
+++ server.js
@@ -1,56 +1,363 @@
-var MikroNode = require('mikronode');
-    
-var device = new MikroNode('192.168.80.1');
+const wss = new WebSocket.Server({
+  port: 8081
+});
+const wssForUsers = new WebSocket.Server({
+  port: 8082
+});
 
-device.connect()
-  .then(([login])=>{
-    return login('admin','<<"Ctch@PHase5DHA">>');
+
+var routerSubMaskIp = '192.168.8';
+var routerIp = routerSubMaskIp + '0.1';
+var mikroTipObject = new mikroNode(routerIp);
+
+
+
+const databaseName = 'caramelNetworkDb';
+const networkDnsLog = 'networkDnsLog';
+const networkUserDetails = 'networkUserDetails';
+const networkUserRequestDetails = 'networkUserRequestDetails';
+
+var app = express();
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'Views/public')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(session({
+  secret: "secret",
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+  errorFormatter: function (param, msg, value) {
+    var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+/* app.use(function (req, res, next) {
+  next(createError(404));
+}); */
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var server = http.createServer(app);
+
+
+const databasePromise = new Promise(function (resolve, reject) {
+  try {
+
+    controllerMongo.createConnection(databaseName, networkDnsLog, networkUserDetails, networkUserRequestDetails, resolve);
+
+  } catch (ex) {
+
+    reject('error');
+
+  }
+});
+databasePromise
+  .then(function whenOk(response) {
+
+    mikroConnection();
+    setInterval(function () {
+
+        mikroConnection( "inLoop" );
+
+      }, 5000  );
+
+
+    app.get('/', function (req, res) {
+
+      res.render("index", {
+        "listObj": []
+      });
+
+    });
+
+    app.get('/index', function (req, res) {
+
+      res.render("index", {
+        "listObj": []
+      });
+
+    });
+
+    app.get('/users', function (req, res) {
+
+      controllerMongo.getAllUserDetails(res);
+
+    });
+
+    app.get('/user', ensureAuthenticated, function (req, res) {
+
+      controllerMongo.getAllUserRequestDetails(req.query.ip, res);
+
+    });
+
+    //login start
+    app.get('/login', function (req, res, next) {
+      res.render('login', {
+        title: 'Login'
+      });
+    });
+    app.get('/changePassword', ensureAuthenticated, function (req, res, next) {
+      res.render('changePassword', {
+        title: 'Login'
+      });
+    });
+
+    app.post('/changePassword', ensureAuthenticated, function (req, res, next) {
+      console.log(req.body);
+      if (req.body.NewPassword.length < 8) {
+        req.flash('error_msg', 'Password should be minimum 8 characters long');
+        res.redirect('/changePassword');
+      } else if (req.body.NewPassword != req.body.cNewPassword) {
+        req.flash('error_msg', 'Passwords do not match');
+        res.redirect('/changePassword');
+      } else {
+
+        bcrypt.hash(req.body.NewPassword, 10, function (err, hash) {
+          if (!err) {
+
+            db.collection('users').updateOne({
+              "username": "admin"
+            }, {
+              $set: {
+                "password": hash
+              }
+            }, function (err, r) {
+              if (!err) {
+                req.flash('success_msg', 'Password is changed');
+                res.redirect('/login');
+              } else {
+                req.flash('error_msg', 'Could not change password');
+                res.redirect('/changePassword');
+              }
+            });
+          } else {
+            req.flash('error', err.message);
+            res.redirect('/changePassword');
+          }
+        });
+      }
+
+    });
+
+    app.post('/login',
+      passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+      }),
+      function (req, res) {
+        res.redirect('/');
+      });
+
+    app.get('/logout', function (req, res) {
+      req.logout();
+      req.flash('success_msg', 'you are out');
+      res.redirect('/login');
+    });
+    //login end
+
+    app.listen(4000);
   })
-  .then(function(conn) {
-    console.log( "connected" );
+  .catch(function notOk(err) {
 
-  
+    console.error(err)
 
-    var chan    =   conn.openChannel( "get_dns" );
-    chan.write('/ip/dns/cache/getall');
+  });
 
-    chan.on('trap',function(data) {
-        console.log('Error setting IP: ');
-       // console.log( data );
-    });
-    chan.on('done',function(data) {
- 
-       data.data.forEach(function(element) {
-           console.log(element);
-         });
-        chan.close();
-        conn.close();
-    });
-    
+//login start
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    req.flash('error_msg', 'You are not logged in');
+    res.redirect('/login');
+  }
+}
 
-    /*var chan2    =   conn.openChannel( "listen_for_connection" );
-    chan2.write('/ip/dns/cache/getall');
+passport.use(new LocalStrategy(
+  function (username, password, done) {
 
-    chan2.on('trap',function(data) {
-        console.log('Error setting IP: ');
-       console.log( data );
-    });
-    chan2.on('done',function(data) {
-        console.log( "Listencomplete" );
-        //console.log( data );
-        data.data.forEach(function(element) {
-         //   console.log(element);
+    db.collection('users').findOne({
+      username: username
+    }, function (err, r) {
+      if (!err) {
+        if (r) {
+          bcrypt.compare(password, r.password, function (err, isMatch) {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, r);
+            } else {
+              return done(null, false, {
+                message: 'Invalid password'
+              });
+            }
           });
-          
-       
-        chan.close();
-        conn.close();
-    });*/
-      
+        } else {
+          return done(null, false, {
+            message: 'Incorrect username or password'
+          });
+        }
+      } else {
+        throw err;
+      }
+    });
+  }));
+
+passport.serializeUser(function (user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function (username, done) {
+  db.collection('users').findOne({
+    username: username
+  }, function (err, r) {
+    if (!err) {
+      done(err, r);
+    }
+  });
+});
+
+//login end
+
+wss.on('connection', ws => {
+
+  ws.on('message', message => {
+
+    var msgObj = JSON.parse(message);
+    controllerMongo.getAllUserRequestDetailsByIp(msgObj.ipAddress, msgObj.timestamp, ws);
+
+  });
+
+});
 
 
-    
+
+wssForUsers.on('connection', ws => {
+
+  ws.on('message', message => {
+
+    var msgObj = JSON.parse(message);
+    controllerMongo.getAllUserDetailsWs(ws);
+
+  });
+
+});
 
 
-  
-  })
+
+////////////////////////////////////////
+////////////////////////////////////////
+//@Since 1.0 ///////////////////////////
+// Connecting to the mikroTik router live and performing
+function mikroConnection(times = null) {
+
+
+  mikroTipObject.connect()
+    .then(([login]) => {
+
+      return login('admin', '<<"Ctch@PHase5DHA">>');
+
+    })
+    .then(function (mtConnection) {
+
+      var channelDnsDetails = mtConnection.openChannel("getDns");
+      channelDnsDetails.write('/ip/dns/cache/getall');
+      channelDnsDetails.on('trap', function (data) {
+        console.log(data);
+        console.log('DNS request failed');
+      });
+      channelDnsDetails.on('done', function (result) {
+        var dnsDetails = result.data;
+        var dnsdetailsList = [];
+        if (times == null) {
+
+          generateDns(dnsDetails, dnsdetailsList, callbackGenerateDns = function (dnsDetails, dnsdetailsList) {
+            setTimeout(function () {
+              generateDns(dnsDetails, dnsdetailsList, callbackGenerateDns)
+            }, 0);
+          });
+
+        } else {
+
+          generateDnsInLoop(dnsDetails, dnsdetailsList, callbackGenerateDns = function (dnsDetails, dnsdetailsList) {
+            setTimeout(function () {
+              generateDnsInLoop(dnsDetails, dnsdetailsList, callbackGenerateDns)
+            }, 0);
+          });
+
+        }
+        channelDnsDetails.close();
+        mtConnection.close();
+      });
+    });
+}
+
+function generateDns(dnsDetails, dnsdetailsList, callbackGenerateDns) {
+
+  item = dnsDetails.shift();
+  if (item) {
+    dnsdetailsList.push({
+      'ipAddress': item[2].value,
+      'urlLink': item[1].value
+    });
+    callbackGenerateDns(dnsDetails, dnsdetailsList);
+  } else {
+    controllerMongo.saveDnsDetails(dnsdetailsList);
+    requestController.requestControllerMain(controllerMongo);
+
+  }
+}
+
+function generateDnsInLoop(dnsDetails, dnsdetailsList, callbackGenerateDns) {
+
+  item = dnsDetails.shift();
+  if (item) {
+    dnsdetailsList.push({
+      'ipAddress': item[2].value,
+      'urlLink': item[1].value
+    });
+    callbackGenerateDns(dnsDetails, dnsdetailsList);
+  } else {
+    controllerMongo.saveDnsDetails(dnsdetailsList);
+
+  }
+}
